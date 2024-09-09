430019ad62814fc28cb6babab34fc0f5432221ad
Itay Maman <94941+imaman@users.noreply.github.com>
2024-09-09 15:22:22 +0300

    use it

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import { DappWalletView } from './dapp-wallet-view';
 
 const App: React.FC = () => {
   return (
     <div className="app">
-      <h1>_Hello, World**</h1>
+      <h1>CDP quickstart</h1>
+      <DappWalletView />
     </div>
   );
 };
